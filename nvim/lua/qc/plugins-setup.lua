74ca0e85eea6b5c92215d9c812f5c356b584c60b
QC Nguyen <codingmass@live.com>
2022-10-25 16:16:15 +0700

    add Markdown Preview

--- nvim/lua/qc/plugins-setup.lua
+++ nvim/lua/qc/plugins-setup.lua
@@ -97,6 +97,23 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- install without yarn or npm
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
